10caae3859c4efddc4afc8fa4ec691fceeb52313
Anh Le <ta.lepham16@gmail.com>
2019-02-21 02:07:30 +0100

    revert

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { WebView, StyleSheet, Platform } from "react-native";
+import { WebView, Platform } from "react-native";
 import PropTypes from "prop-types";
 /**
  * initial script
@@ -17,8 +17,6 @@ var settingChartScript = `
 	
 	var canvasEl = document.createElement("canvas");
 	canvasEl.setAttribute("id", "myChart");
-	canvasEl.setAttribute("height", {HEIGHT});
-	canvasEl.setAttribute("width", {WIDTH});
 	document.getElementById("body").appendChild(canvasEl);
 	
 	var ctx = document.getElementById("myChart").getContext('2d');
@@ -26,90 +24,87 @@ var settingChartScript = `
 `;
 
 export default class Chart extends Component {
-  static propTypes = {
-    /**
-     * props
-     * @type {[object]}
-     */
-    chartConfiguration: PropTypes.object.isRequired,
-    defaultFontSize: PropTypes.number,
-    height: PropTypes.string,
-    width: PropTypes.string
-  };
-  constructor(props) {
-    super(props);
-  }
-  componentWillReceiveProps(nextProps) {
-    if (
-      nextProps.chartConfiguration !== this.props.chartConfiguration ||
-      nextProps.defaultFontSize !== this.props.defaultFontSize
-    ) {
-      this.setChart(nextProps.chartConfiguration, nextProps.defaultFontSize);
-    }
-  }
-  setChart(chartConfiguration, defaultFontSize, height, width) {
-    if (
-      !chartConfiguration ||
-      undefined == defaultFontSize ||
-      null == defaultFontSize
-    ) {
-      return;
-    }
-    this.webview &&
-      this.webview.injectJavaScript(
-        settingChartScript
-          .replace("{CONFIG}", JSON.stringify(chartConfiguration))
-          .replace("{DEFAULT_FONT_SIZE}", defaultFontSize)
-          .replace("{HEIGHT}", height)
-          .replace("{WIDTH}", width)
-      );
-  }
+	static propTypes = {
+		/**
+		 * props
+		 * @type {[object]}
+		 */
+		chartConfiguration: PropTypes.object.isRequired,
+		defaultFontSize: PropTypes.number,
+		height: PropTypes.string,
+		width: PropTypes.string,
+	};
+	constructor(props) {
+		super(props);
+	}
+	componentWillReceiveProps(nextProps) {
+		if (
+			nextProps.chartConfiguration !== this.props.chartConfiguration ||
+			nextProps.defaultFontSize !== this.props.defaultFontSize
+		) {
+			this.setChart(nextProps.chartConfiguration, nextProps.defaultFontSize);
+		}
+	}
+	setChart(chartConfiguration, defaultFontSize) {
+		if (
+			!chartConfiguration ||
+			undefined == defaultFontSize ||
+			null == defaultFontSize
+		) {
+			return;
+		}
+		this.webview &&
+			this.webview.injectJavaScript(
+				settingChartScript
+					.replace("{CONFIG}", JSON.stringify(chartConfiguration))
+					.replace("{DEFAULT_FONT_SIZE}", defaultFontSize)
+			);
+	}
 
-  render() {
-    const defaultFontSize = this.props.defaultFontSize
-      ? this.props.defaultFontSize
-      : 12;
-    const defaultHeight = this.props.height ? this.props.height : "250px";
-    const defaultWidth = this.props.width ? this.props.width : "300px";
-    return (
-      <WebView
-        style={{ flex: 1 }}
-        ref={ref => (this.webview = ref)}
-        injectedJavaScript={settingChartScript
-          .replace("{CONFIG}", JSON.stringify(this.props.chartConfiguration))
-          .replace("{DEFAULT_FONT_SIZE}", defaultFontSize)
-          .replace("{HEIGHT}", defaultHeight)
-          .replace("{WIDTH}", defaultWidth)}
-        source={require("./dist/index.html")}
-        onError={error => {
-          console.log(error);
-        }}
-        renderError={e => {
-          //Renders this view while resolving the error
-          return (
-            <View>
-              <Text> {e} </Text>
-              <Text> Restart the App if the error persists. </Text>
-              <ActivityIndicator
-                animating={true}
-                color="#94C11F"
-                size="large"
-                hidesWhenStopped={true}
-                style={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                  padding: 30,
-                  flex: 1
-                }}
-              />
-            </View>
-          );
-        }}
-        // scalesPageToFit false for IOS and true for Android
-        scalesPageToFit={Platform.OS === "ios" ? false : true}
-        bounces={false}
-        style={this.props.style}
-      />
-    );
-  }
+	render() {
+		const defaultFontSize = this.props.defaultFontSize
+			? this.props.defaultFontSize
+			: 12;
+		return (
+			<WebView
+				style={{ flex: 1 }}
+				ref={ref => (this.webview = ref)}
+				injectedJavaScript={settingChartScript
+					.replace("{CONFIG}", JSON.stringify(this.props.chartConfiguration))
+					.replace("{DEFAULT_FONT_SIZE}", defaultFontSize)}
+				source={require("./dist/index.html")}
+				onError={error => {
+					console.log(error);
+				}}
+				renderError={e => {
+					//Renders this view while resolving the error
+					return (
+						<View>
+							<Text> {e} </Text>
+							<Text>
+								{" "}
+								Restart the App if the error persists or try again later.{" "}
+							</Text>
+							<ActivityIndicator
+								animating={true}
+								color="#94C11F"
+								size="large"
+								hidesWhenStopped={true}
+								style={{
+									alignItems: "center",
+									justifyContent: "center",
+									padding: 30,
+									flex: 1,
+								}}
+							/>
+						</View>
+					);
+				}}
+				// scalesPageToFit false for IOS and true for Android
+				scalesPageToFit={Platform.OS === "ios" ? false : true}
+				bounces={false}
+				style={this.props.style}
+			/>
+		);
+	}
 }
